6241aa3d46b1b20fec75923915a02ec18a32ba48
Michael Schenck <mschenck@gmail.com>
2011-04-02 21:27:35 -0400

    Now building hashes and arrays safely

--- lib/terminus.rb
+++ lib/terminus.rb
@@ -1,6 +1,34 @@
 require './lib/couch'
 require 'json'
 
+def try_string_to_hash(value)
+  value_array = []
+  
+  # Load up array if possible
+  value.scan(/"(?:[^"\\]|\\.)*"/) { |array_element|
+    array_element.gsub!(/\"/,"")
+    value_array.push(array_element)
+  }
+  
+  # If array, lock for hash_values
+  value_hash = {}
+  value_array.each_index do |key|
+    if value_array[key].match(/(.)* => (.)*/)
+      new_key, new_val = value_array[key].split(/[ :]=>[ :]/)
+      value_hash[new_key] = new_val
+    end
+  end
+  
+  if value_hash.length > 1
+    return value_hash
+  elsif value_array.length > 1
+    return value_array
+  end
+
+  value
+end
+
+
 get '/terminus/' do
   server = Couch::Server.new("localhost", "5984")
 
@@ -25,12 +53,9 @@ get '/terminus/' do
     @attributes.each do |attr|
       key = attr[0]
       value = attr[1]
-      
-      # If Array or Hash, convert
-      if value.match('^\[') or value.match('^\{')
-        value = eval(value)
-      end
-      
+
+      value = try_string_to_hash value
+              
       node["parameters"].push( {key => value} )
     end
     
